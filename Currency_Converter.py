7890efa1e523d98887f37554a595a55dcab49d4a
Harsha100722 <harshavardhanaadi464@gmail.com>
2025-05-17 19:26:30 +0530

    sixth commit

--- Currency_Converter.py
+++ Currency_Converter.py
@@ -8,27 +8,29 @@ root=tk.Tk()
 root.title("currency converter")
 root.geometry("500x500")
 
-Label(root,text="CURRENCY CONVERTER",font="Times 20 bold",bg="white",fg="red").place(x=70,y=0)
-Label(root,text="Enter Your Amount:",font="Times 15 bold",bg="white",fg="black").place(x=40,y=75)
-Label(root,text="Enter Currency Type:",font="Times 15 bold",bg="white",fg="black").place(x=40,y=125)
-Label(root,text="Enter Target Currency:",font="Times 15 bold",bg="white",fg="black").place(x=40,y=175)
+Label(root,text="CURRENCY CONVERTER",font="Times 20 bold",bg="white",fg="red").place(x=100,y=0)
+logo=tk.PhotoImage(file="Currency.gif")
+Label(root,image=logo,bg="white").place(x=200,y=50)
+Label(root,text="Enter Your Amount:",font="Times 15 bold",bg="white",fg="black").place(x=40,y=165)
+Label(root,text="Enter Currency Type:",font="Times 15 bold",bg="white",fg="black").place(x=40,y=220)
+Label(root,text="Enter Target Currency:",font="Times 15 bold",bg="white",fg="black").place(x=40,y=275)
 
 global e1
 global e2
 global e3
 
 e1=Entry(root,width=22)
-e1.place(x=222,y=82)
+e1.place(x=222,y=172)
 
 e2=tk.StringVar()
 curr1=ttk.Combobox(root,width=20,text=e2)
 curr1['values']=('USD', 'EUR', 'INR', 'GBP', 'JPY', 'AUD', 'CAD', 'CHF', 'CNY', 'NZD', 'SGD', 'ZAR', 'BRL', 'RUB', 'MXN', 'SEK', 'NOK', 'DKK', 'HKD', 'KRW', 'MYR', 'THB', 'IDR', 'PHP', 'AED', 'SAR', 'EGP', 'TRY', 'PLN', 'CZK', 'HUF')
-curr1.place(x=237,y=130)
+curr1.place(x=237,y=226)
 
 e3=tk.StringVar()
 curr2=ttk.Combobox(root,width=20,text=e3)
 curr2['values']=('USD', 'EUR', 'INR', 'GBP', 'JPY', 'AUD', 'CAD', 'CHF', 'CNY', 'NZD', 'SGD', 'ZAR', 'BRL', 'RUB', 'MXN', 'SEK', 'NOK', 'DKK', 'HKD', 'KRW', 'MYR', 'THB', 'IDR', 'PHP', 'AED', 'SAR', 'EGP', 'TRY', 'PLN', 'CZK', 'HUF')
-curr2.place(x=252,y=180)
+curr2.place(x=252,y=280)
 
 result=None
 
@@ -56,11 +58,11 @@ def conv():
         cur=round(c.convert(a1,a2,a3),2)
         if result==None:
             result=Label(root,text=(f"{a1} {a2} = {cur} {a3}"),font="Times 13 bold",fg="black")
-            result.place(x=145,y=278)
+            result.place(x=180,y=370)
         else:
             result.config(text=(f"{a1} {a2} = {cur} {a3}"))
         return
 
-Button(root,text="CONVERT",command=conv,font="Times 15 bold",bg="white",fg="black").place(x=200,y=225)
+Button(root,text="CONVERT",command=conv,font="Times 15 bold",bg="white",fg="black").place(x=200,y=320)
 
 root.mainloop()
